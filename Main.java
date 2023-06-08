32f71763e3c69053c16632ac610606cfbf6a2944
DzKy962 <127397723+DzKy962@users.noreply.github.com>
2023-06-08 17:24:04 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,57 +1,44 @@
-// import java.io.*;
 import java.util.*;
 
-class BFSTraversal {
-    private int node;
+class DFSTraversal {
     private LinkedList<Integer> adj[];
-    private Queue<Integer> que;
+    private boolean visited[];
 
-    BFSTraversal(int v) {
-        node = v;
-        adj = new LinkedList[node];
-        for (int i = 0; i < v; i++) {
-            adj[i] = new LinkedList<>();
-        }
-        que = new LinkedList<Integer>();
+    DFSTraversal(int V){
+        adj = new LinkedList[V];
+        visited = new boolean[V];
+        for (int i=0; i<V; i++)
+            adj[i] = new LinkedList<Integer>();
     }
 
-    void insertEdge(int v, int w) {
-        adj[v].add(w);
+    void insertEdge(int src, int dest){
+        adj[src].add(dest);
     }
 
-    void BFS(int n) {
-        boolean nodes[] = new boolean[node];
-        int a = 0;
-        nodes[n] = true;
-        que.add(n);
-        while (que.size() != 0) {
-            n = que.poll();
-            System.out.println(n + " ");
-            for (int i = 0; i < adj[n].size(); i++) {
-                a = adj[n].get(i);
-                if (!nodes[a]) {
-                    nodes[a] = true;
-                    que.add(a);
-                }
-            }
+    void DFS(int vertex){
+        visited[vertex] = true;
+        System.out.print(vertex + " ");
+        Iterator<Integer> it = adj[vertex].listIterator();
+        while (it.hasNext()){
+            int n = it.next();
+            if (!visited[n])
+                DFS(n);
         }
     }
 
-    public static void main(String args[]) {
-        BFSTraversal graph = new BFSTraversal(6);
+    public static void main(String[] args) {
+        DFSTraversal graph = new DFSTraversal(8);
         graph.insertEdge(0, 1);
+        graph.insertEdge(0, 2);
         graph.insertEdge(0, 3);
-        graph.insertEdge(0, 4);
-        graph.insertEdge(4, 5);
+        graph.insertEdge(1, 3);
+        graph.insertEdge(2, 4);
         graph.insertEdge(3, 5);
-        graph.insertEdge(1, 2);
-        graph.insertEdge(1, 0);
-        graph.insertEdge(2, 1);
-        graph.insertEdge(4, 1);
-        graph.insertEdge(3, 1);
-        graph.insertEdge(5, 4);
-        graph.insertEdge(5, 3);
-        System.out.println("Breadth First Traversal for the graph is: ");
-        graph.BFS(0);
+        graph.insertEdge(3, 6);
+        graph.insertEdge(4, 7);
+        graph.insertEdge(4, 5);
+        graph.insertEdge(5, 2);
+        System.out.println("Depth First Traversal for the graph is: ");
+        graph.DFS(0);
     }
 }
